1bb8c0473cdcd21309daa2703bd0c63ecfd282e8
Marco Cusinato <marco.cusinato@uv.es>
2025-06-23 16:17:53 +0200

    Test...

--- pycbc/workflow/grb_utils.py
+++ pycbc/workflow/grb_utils.py
@@ -583,9 +583,8 @@ def make_pygrb_plot(workflow, exec_name, out_dir,
         if len(tags) > 2:
             node.add_opt('--cbar-variable', tags[2])
     elif exec_name == 'pycbc_plot_bank_corner':
-        node.add_raw_arg('--parameters')
-        for param in workflow.cp.options(exec_name):
-            node.add_raw_arg(param)
+        params = workflow.cp.options(exec_name)
+        node.add_list_opt('--parameters', params)
         node.add_opt('--no-suptitle')
         node.add_opt('--title', f"{tags[0]} injections")
 
